ff1e05131ec61203b4a3baf6d7e832f05dcefa02
Josh Augustin <augustinspring@gmail.com>
2013-04-27 17:49:30 -0700

    Introduces cycle notation compatibility, permutation composition, and the .cycle command.

--- Permutation.js
+++ Permutation.js
@@ -1,18 +1,18 @@
 /* This class represents a permutation, and encapsulates
  * some useful behaviors for dealing with them, such as
  * cloning and toString.
+ *
+ * Now, you can compose permutations with .compose(), and 
+ * intialize them from cycle notation with Permutation.fromCycleString().
  * 
- * Coming Soon:
- *  - Cycle notation compatibility, both through a
- *  Permutation.fromCycleString function and a .toCycleString
- *  function. These will be compatible with nondisjoint permutations.
- *  - Permutation composition, resulting in a new permutation,
- *  to be implemented with a .compose function.
- *  - A .cycles property which consists in an array of cycles
- *  of this permutation, indexed by element. Thus .cycles[0] gives
- *  the cycle containing 0.
+ * The cycles of a permutation can be accessed with the .cycles property,
+ * which is a lazy initializer pattern which returns an array of subcycles,
+ * each of which is an array.
  *
+ * The .toCycleString() method prints a nice cycle representation of the permutation.
  *
+ * I still need to find a nice way of making these permutations immutable, though,
+ * since changing the value of any of their indicies would break the class.
  */
 
 
@@ -35,7 +35,10 @@ Array.prototype.hasDuplicates = function() {
   }
   return false;
 }
-
+//
+// An easy foreach construct with Arrays. Functions
+// passed to be executed are passed two arguments,
+// func(value, index), for each element in the array.
 Array.prototype.forEach = function(func) {
   for (var i = 0; i < this.length; ++i) {
     func(this[i], i);
@@ -90,6 +93,38 @@ Permutation = function(arr) {
   for (var i = 0; i < arr.length; ++i) {
     this[PERMUTATION_INDEX + i] = arr[i];
   }
+
+  var _cycles = undefined;
+  var getCycles = function(perm) {
+    var already_used = {};
+
+    var cycles = [];
+
+    for (var i = 0; i < perm.length; ++i) {
+      if (perm[i] != i && already_used[i] == undefined) {
+        var curr_cycle = [i];
+        already_used[i] = true;
+
+        var j = perm[i];
+        while (j != i) {
+          curr_cycle.push(j);
+          already_used[j] = true;
+          j = perm[j];
+        }
+
+        cycles.push(curr_cycle);
+      } 
+    }
+
+    return cycles;
+  }
+  this.__defineGetter__("cycles", function() {
+    if (_cycles == undefined) {
+      _cycles = getCycles(this);
+    }
+
+    return _cycles;
+  });
 }
 
 Permutation.fromFunction = function(func, n) {
@@ -110,10 +145,20 @@ Permutation.prototype.toString = function() {
   var total = [];
 
   for (var i = PERMUTATION_INDEX; i < this.length + PERMUTATION_INDEX; ++i) {
-    total.push(this.getChar(this[i]));
+    total.push(Permutation.getChar(this[i]));
   }
 
   return total.join('');
+}
+
+Permutation.prototype.toCycleString = function() {
+  var result = "";
+
+  for (var i = 0; i < this.cycles.length; ++i) {
+    result += '(' + this.cycles[i].join('') + ')';
+  }
+
+  return result;
 }
 
 Permutation.prototype.clone = function() {
@@ -128,6 +173,22 @@ Permutation.prototype.clone = function() {
 
 Permutation.prototype.at = function(i) {
   return this[i+PERMUTATION_INDEX];
+}
+
+// Returns a new Permutation consisting of theta
+// composed with this, i.e. this(theta)
+Permutation.prototype.compose = function(theta) {
+  if (theta.length != this.length) {
+    return undefined;
+  }
+
+  var arr = [];
+
+  for (var i = 0; i < this.length; ++i) {
+    arr[i] = this[theta[i]];
+  }
+
+  return new Permutation(arr);
 }
 
 Permutation.equals = function(left, right) {
@@ -163,7 +224,7 @@ var MAX_ALLOWED_PERMUTATION_INDEX = 35;
 // to the following pattern:
 //    If i = 0..9, then return '0'..'9' 
 //    If i = 10..35 then return 'A'..'Z'
-Permutation.prototype.getChar = function(i) {
+Permutation.getChar = function(i) {
   if (i >= 0 && i <= 9) {
     return String.fromCharCode(C0 + i);
   } else if (i <= MAX_ALLOWED_PERMUTATION_INDEX) {
@@ -216,4 +277,61 @@ Permutation.fromString = function(s) {
 
     return new Permutation(arr);
   }
+}
+
+Permutation.fromCycleString = function(s, n) {
+  function splitCycles(s) {
+    var result = [];
+
+    for (var i = s.indexOf('('); i>=0; i = s.indexOf('(', i+1)) {
+      var j = s.indexOf(')', i);
+      if (j > 0 && j > i + 2) {
+        result.push(s.substring(i+1,j));
+      }
+    }
+
+    return result;
+  }
+  function followCycles(cycles, char) {
+    var value = char;
+    for (var i = cycles.length - 1; i >= 0; --i) {
+      var index = cycles[i].indexOf(value);
+      if (index >= 0) {
+        if (index < cycles[i].length - 1) {
+          value = cycles[i][index+1];
+        } else {
+          value = cycles[i][0];
+        }
+      }
+    }
+
+    return value;
+  }
+
+  function getHighestChar(cycles) {
+    var highest = 0;
+
+    for (var i = 0; i < cycles.length; ++i) {
+      for (var j = 0; j < cycles[i].length; ++j) {
+        var curr_code = Permutation.parseChar(cycles[i][j]);
+        if (curr_code > highest) {
+          highest = curr_code;
+        }
+      }
+    }
+
+    return highest;
+  }
+
+  var splits = splitCycles(s);
+  if (n == undefined) {
+    n = getHighestChar(splits);
+  }
+
+  var arr = [];
+  for (var i = 0; i <= n; ++i) {
+    arr.push(Permutation.parseChar(followCycles(splits, Permutation.getChar(i))));
+  }
+  
+  return new Permutation(arr);
 }
